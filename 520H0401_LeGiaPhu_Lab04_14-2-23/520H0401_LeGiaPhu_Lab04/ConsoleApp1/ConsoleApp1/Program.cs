c299f1113a07d14b012d285ccc981839217ac308
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2023-02-16 23:50:47 +0700

    lab04

---
+++ 520H0401_LeGiaPhu_Lab04_14-2-23/520H0401_LeGiaPhu_Lab04/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolDBEntities db = new SchoolDBEntities();
+            var student = db.Students.Where(s => s.StudentID == 6).FirstOrDefault();
+            Console.WriteLine(student.StudentID);
+            Console.WriteLine(student.StudentName);
+            Console.WriteLine(student.StudentAddress);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 520H0401_LeGiaPhu_Lab04_14-2-23/520H0401_LeGiaPhu_Lab04/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SchoolDBEntities db = new SchoolDBEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SchoolDBEntities db = new SchoolDBEntities();
+
+            //var student = db.Students.Where(x => x.StudentID == 1).FirstOrDefault();
+            ////textBox1.Text = student.StudentName.ToString();
+
+            //Student std = new Student { 
+            //    StudentID = 100,
+            //    StudentName = "John",  
+            //};
+            ////textBox1.Text = std.StudentName.ToString();
+            ////db.Students.Add(std);
+            ////db.SaveChanges();
+            ///
+            Load_Data();
+        }
+
+        private void Load_Data()
+        {
+            try
+            {
+                var student = db.Students.Select(s => s).ToList();
+                dataGridView1.DataSource = student;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
